b45af987c4a690c56e87faf4c20e205b2ad1f88e
Silvia Michele <silvia.teles.br@gmail.com>
2021-10-17 14:54:09 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,24 @@
+var numero_sorteado;
+var array_numeros = Array();
+function sortearNumero(){
+	numero_sorteado = parseInt(Math.random()*100);
+}
+sortearNumero();
+function enviar(){
+	let numero_digitado = document.getElementById("input").value;
+	array_numeros.push(numero_digitado);
+	if (numero_digitado == numero_sorteado){
+		array_numeros = Array();
+		document.getElementById("status").innerHTML = "acertou";
+		document.getElementById("paragrafo").innerHTML = "";
+	}
+	else{
+		document.getElementById("paragrafo").innerHTML = "Números digitados: " + array_numeros.join(", ");
+		if (numero_digitado > numero_sorteado){
+			document.getElementById("status").innerHTML = "maior";
+		}
+		else{
+			document.getElementById("status").innerHTML = "menor";
+		}
+	}
+}
