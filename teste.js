2c3ef9fbdf1f736224afca043da2aa786c279798
Silvia Michele <silvia.teles.br@gmail.com>
2021-10-20 10:58:44 -0300

    css do input

--- teste.js
+++ teste.js
@@ -1,24 +1,25 @@
 var numero_sorteado;
 var array_numeros = Array();
-function sortearNumero(){
-	numero_sorteado = parseInt(Math.random()*100);
+
+function sortearNumero() {
+    numero_sorteado = parseInt(Math.random() * 100);
 }
 sortearNumero();
-function enviar(){
-	let numero_digitado = document.getElementById("input").value;
-	array_numeros.push(numero_digitado);
-	if (numero_digitado == numero_sorteado){
-		array_numeros = Array();
-		document.getElementById("status").innerHTML = "acertou";
-		document.getElementById("paragrafo").innerHTML = "";
-	}
-	else{
-		document.getElementById("paragrafo").innerHTML = "Números digitados: " + array_numeros.join(", ");
-		if (numero_digitado > numero_sorteado){
-			document.getElementById("status").innerHTML = "maior";
-		}
-		else{
-			document.getElementById("status").innerHTML = "menor";
-		}
-	}
+
+function enviar() {
+    let numero_digitado = document.getElementById("input").value;
+    array_numeros.push(numero_digitado);
+    if (numero_digitado == numero_sorteado) {
+        array_numeros = Array();
+        document.getElementById("status").innerHTML = "acertou";
+        document.getElementById("paragrafo").innerHTML = "";
+    } else {
+        document.getElementById("paragrafo").innerHTML = "Números digitados: " + array_numeros.join(", ");
+        if (numero_digitado > numero_sorteado) {
+            document.getElementById("status").innerHTML = "maior";
+        } else {
+            document.getElementById("status").innerHTML = "menor";
+        }
+    }
+    document.getElementById("input").value = "";
 }
